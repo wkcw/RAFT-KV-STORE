8a55c14cf3e8c739154bd3e912c0dfcb1ca39577
wkcw <594877110@qq.com>
2019-04-23 16:33:01 -0700

    add AppendEntries

--- src/service/log.go
+++ src/service/log.go
@@ -1,17 +1,28 @@
 package service
 
+import(
+	pb "proto"
+)
+
 type Log struct{
 	EntryList []entry
 	lastIndex int // initial value is 1 not 0 according to paper
+	commitIndex int64
 
 }
 
 type entry struct{
 	op string //put only?
 	val string // e.g. op=put val=key1:v1
-	term int
+	term int64
 }
 
-func (log *Log) cutEntries(cutStartIndex int){
+func (log *Log) cutEntries(cutStartIndex int64){
 	log.EntryList = log.EntryList[0:cutStartIndex]
+}
+
+func (log *Log) appendEntries(appendStartIndex int64, reqEntries []*pb.Entry){
+	for i, reqEntry := range reqEntries{
+		log.EntryList[appendStartIndex+int64(i)] = entry{op:reqEntry.Op, val:reqEntry.Val, term:reqEntry.Term}
+	}
 }
--- src/service/raftserver.go
+++ src/service/raftserver.go
@@ -1,11 +1,46 @@
 package service
 
+import(
+	pb "proto"
+	"context"
+)
 
 type RaftService struct{
-
+	currentTerm int64
+	log Log
 }
 
-func (*RaftService) AppendEntries(){
+
+func (myRaft *RaftService) AppendEntries(ctx context.Context, req pb.AERequest) (*pb.AEResponse, error){
+	response := &pb.AEResponse{Term:myRaft.currentTerm}
+	//rule 1
+	if req.Term < myRaft.currentTerm {
+		response.Success = false
+		return response, nil
+	}
+	//rule 2
+	if len(myRaft.log.EntryList) < int(req.PrevLogIndex) || myRaft.log.EntryList[req.PrevLogIndex-1].term != req.PrevLogTerm{
+		response.Success = false
+		return response, nil
+
+	}
+	appendStartIndex := int64(req.PrevLogIndex)
+	//rule 3
+	for _, reqEntry := range req.Entries{
+		if myRaft.log.EntryList[appendStartIndex].term != reqEntry.Term {
+			myRaft.log.cutEntries(appendStartIndex)
+			break
+		}
+		appendStartIndex++
+	}
+	//rule 4
+	myRaft.log.appendEntries(appendStartIndex, req.Entries)
+	//rule 5
+	if req.LeaderCommit > myRaft.log.commitIndex {
+		myRaft.log.commitIndex = minInt64(req.LeaderCommit, int64(len(myRaft.log.EntryList)))
+	}
+	response.Success = true
+	return response, nil
 
 }
 
@@ -41,5 +76,14 @@ func (*RaftService) checkGotVotes(){
 
 func (*RaftService) checkTerm(){
 	return term
+}
+
+
+func minInt64(num1 int64, num2 int64) int64{
+	if num1<num2 {
+		return num1
+	}else{
+		return num2
+	}
 }
 
