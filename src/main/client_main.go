a01bf57b002d683d255e9f5d416f526a852682b7
cpwang96 <38072468+cpwang96@users.noreply.github.com>
2019-05-01 15:18:00 -0700

    Change client's main

--- src/main/client_main.go
+++ src/main/client_main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"client"
+	"google.golang.org/grpc"
 	"log"
+	"time"
 	"util"
 	"fmt"
+	pb "proto"
+	"context"
 )
 
 var (
@@ -22,12 +26,45 @@ func main() {
 		fmt.Scanln(&operation, &key, &value)
 		//operation = "put"
 		if (operation == "put") {
-			r, err := client.PutAndBroadcast(key, value)
-			if err != nil {
-				log.Printf("could not put: %v", err)
-			}
-			if r != nil{
-				log.Printf("Return code: %s", r.Ret)
+			var address string
+			address = client.PickRandomServer()
+			for {
+				// connect to the server
+				conn, err := grpc.Dial(address, grpc.WithInsecure())
+				if err != nil {
+					log.Fatalf("did not connect: %v", err)
+				}
+				defer conn.Close()
+				c := pb.NewKeyValueStoreClient(conn)
+
+				// Contact the server and print out its response.
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+				response, errCode := c.PutRaft(ctx, &pb.PutRaftRequest{Key: key, Value: value})
+				if errCode != nil {
+					log.Printf("could not put raft, an timeout occurred: %v", err)
+				}
+				if response.Ret == pb.ReturnCode_FAILURE_NOTLEADER {
+					// if the return address is not leader
+					leaderID := response.LeaderID
+					leaderServer := client.ServerList.Servers[leaderID]
+					address = leaderServer.Host + ":" + leaderServer.Port
+					continue;
+				}
+
+				if response.Ret == pb.ReturnCode_SUCCESS {
+					ret, err := c.Put(ctx, &pb.PutRequest{Key: key, Value: value})
+					if (err != nil) {
+						log.Printf("could not put to the leader, an timeout occurred: %v", err)
+					}
+					if ret.Ret == pb.ReturnCode_SUCCESS {
+						log.Print("Put succeeded")
+						break;
+					}
+					// put to the leader unsuccessful
+					continue;
+				}
+
 			}
 		}
 
