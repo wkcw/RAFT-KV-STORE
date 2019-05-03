1812ef67253aaec71e07e625fcc44505ebb7e7ba
cpwang96 <38072468+cpwang96@users.noreply.github.com>
2019-05-03 16:29:14 -0700

    fix monkey proto

--- src/chaosmonkey/chaosmonkey.pb.go
+++ src/chaosmonkey/chaosmonkey.pb.go
@@ -8,6 +8,8 @@ import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -47,6 +49,84 @@ func (StatusCode) EnumDescriptor() ([]byte, []int) {
 	return fileDescriptor_3ee19988c9499698, []int{0}
 }
 
+type Server struct {
+	ServerID             int32    `protobuf:"varint,1,opt,name=serverID,proto3" json:"serverID,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Server) Reset()         { *m = Server{} }
+func (m *Server) String() string { return proto.CompactTextString(m) }
+func (*Server) ProtoMessage()    {}
+func (*Server) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3ee19988c9499698, []int{0}
+}
+
+func (m *Server) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Server.Unmarshal(m, b)
+}
+func (m *Server) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Server.Marshal(b, m, deterministic)
+}
+func (m *Server) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Server.Merge(m, src)
+}
+func (m *Server) XXX_Size() int {
+	return xxx_messageInfo_Server.Size(m)
+}
+func (m *Server) XXX_DiscardUnknown() {
+	xxx_messageInfo_Server.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Server proto.InternalMessageInfo
+
+func (m *Server) GetServerID() int32 {
+	if m != nil {
+		return m.ServerID
+	}
+	return 0
+}
+
+type PartitionInfo struct {
+	Server               []*Server `protobuf:"bytes,1,rep,name=server,proto3" json:"server,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *PartitionInfo) Reset()         { *m = PartitionInfo{} }
+func (m *PartitionInfo) String() string { return proto.CompactTextString(m) }
+func (*PartitionInfo) ProtoMessage()    {}
+func (*PartitionInfo) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3ee19988c9499698, []int{1}
+}
+
+func (m *PartitionInfo) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PartitionInfo.Unmarshal(m, b)
+}
+func (m *PartitionInfo) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PartitionInfo.Marshal(b, m, deterministic)
+}
+func (m *PartitionInfo) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PartitionInfo.Merge(m, src)
+}
+func (m *PartitionInfo) XXX_Size() int {
+	return xxx_messageInfo_PartitionInfo.Size(m)
+}
+func (m *PartitionInfo) XXX_DiscardUnknown() {
+	xxx_messageInfo_PartitionInfo.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PartitionInfo proto.InternalMessageInfo
+
+func (m *PartitionInfo) GetServer() []*Server {
+	if m != nil {
+		return m.Server
+	}
+	return nil
+}
+
 type Status struct {
 	Ret                  StatusCode `protobuf:"varint,1,opt,name=ret,proto3,enum=chaosmonkey.StatusCode" json:"ret,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}   `json:"-"`
@@ -58,7 +138,7 @@ func (m *Status) Reset()         { *m = Status{} }
 func (m *Status) String() string { return proto.CompactTextString(m) }
 func (*Status) ProtoMessage()    {}
 func (*Status) Descriptor() ([]byte, []int) {
-	return fileDescriptor_3ee19988c9499698, []int{0}
+	return fileDescriptor_3ee19988c9499698, []int{2}
 }
 
 func (m *Status) XXX_Unmarshal(b []byte) error {
@@ -97,7 +177,7 @@ func (m *ConnMatrix) Reset()         { *m = ConnMatrix{} }
 func (m *ConnMatrix) String() string { return proto.CompactTextString(m) }
 func (*ConnMatrix) ProtoMessage()    {}
 func (*ConnMatrix) Descriptor() ([]byte, []int) {
-	return fileDescriptor_3ee19988c9499698, []int{1}
+	return fileDescriptor_3ee19988c9499698, []int{3}
 }
 
 func (m *ConnMatrix) XXX_Unmarshal(b []byte) error {
@@ -136,7 +216,7 @@ func (m *ConnMatrix_MatRow) Reset()         { *m = ConnMatrix_MatRow{} }
 func (m *ConnMatrix_MatRow) String() string { return proto.CompactTextString(m) }
 func (*ConnMatrix_MatRow) ProtoMessage()    {}
 func (*ConnMatrix_MatRow) Descriptor() ([]byte, []int) {
-	return fileDescriptor_3ee19988c9499698, []int{1, 0}
+	return fileDescriptor_3ee19988c9499698, []int{3, 0}
 }
 
 func (m *ConnMatrix_MatRow) XXX_Unmarshal(b []byte) error {
@@ -164,6 +244,45 @@ func (m *ConnMatrix_MatRow) GetVals() []float32 {
 	return nil
 }
 
+type ServerStat struct {
+	ServerID             int32    `protobuf:"varint,1,opt,name=serverID,proto3" json:"serverID,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ServerStat) Reset()         { *m = ServerStat{} }
+func (m *ServerStat) String() string { return proto.CompactTextString(m) }
+func (*ServerStat) ProtoMessage()    {}
+func (*ServerStat) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3ee19988c9499698, []int{4}
+}
+
+func (m *ServerStat) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ServerStat.Unmarshal(m, b)
+}
+func (m *ServerStat) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ServerStat.Marshal(b, m, deterministic)
+}
+func (m *ServerStat) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ServerStat.Merge(m, src)
+}
+func (m *ServerStat) XXX_Size() int {
+	return xxx_messageInfo_ServerStat.Size(m)
+}
+func (m *ServerStat) XXX_DiscardUnknown() {
+	xxx_messageInfo_ServerStat.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ServerStat proto.InternalMessageInfo
+
+func (m *ServerStat) GetServerID() int32 {
+	if m != nil {
+		return m.ServerID
+	}
+	return 0
+}
+
 type MatValue struct {
 	Row                  int32    `protobuf:"varint,1,opt,name=row,proto3" json:"row,omitempty"`
 	Col                  int32    `protobuf:"varint,2,opt,name=col,proto3" json:"col,omitempty"`
@@ -177,7 +296,7 @@ func (m *MatValue) Reset()         { *m = MatValue{} }
 func (m *MatValue) String() string { return proto.CompactTextString(m) }
 func (*MatValue) ProtoMessage()    {}
 func (*MatValue) Descriptor() ([]byte, []int) {
-	return fileDescriptor_3ee19988c9499698, []int{2}
+	return fileDescriptor_3ee19988c9499698, []int{5}
 }
 
 func (m *MatValue) XXX_Unmarshal(b []byte) error {
@@ -221,34 +340,42 @@ func (m *MatValue) GetVal() float32 {
 
 func init() {
 	proto.RegisterEnum("chaosmonkey.StatusCode", StatusCode_name, StatusCode_value)
+	proto.RegisterType((*Server)(nil), "chaosmonkey.Server")
+	proto.RegisterType((*PartitionInfo)(nil), "chaosmonkey.PartitionInfo")
 	proto.RegisterType((*Status)(nil), "chaosmonkey.Status")
 	proto.RegisterType((*ConnMatrix)(nil), "chaosmonkey.ConnMatrix")
 	proto.RegisterType((*ConnMatrix_MatRow)(nil), "chaosmonkey.ConnMatrix.MatRow")
+	proto.RegisterType((*ServerStat)(nil), "chaosmonkey.ServerStat")
 	proto.RegisterType((*MatValue)(nil), "chaosmonkey.MatValue")
 }
 
 func init() { proto.RegisterFile("chaosmonkey.proto", fileDescriptor_3ee19988c9499698) }
 
 var fileDescriptor_3ee19988c9499698 = []byte{
-	// 273 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x91, 0x41, 0x4b, 0xc3, 0x40,
-	0x10, 0x85, 0xbb, 0x49, 0x1b, 0x74, 0x22, 0x12, 0x47, 0xa4, 0xa1, 0x88, 0x86, 0x9c, 0xa2, 0x87,
-	0x1c, 0xd2, 0xa3, 0x20, 0x42, 0xf0, 0x24, 0xa1, 0xb0, 0x52, 0x8f, 0xc2, 0x98, 0x04, 0x14, 0xd7,
-	0x4c, 0x48, 0xb6, 0x8d, 0xfe, 0x05, 0x7f, 0xb5, 0x6c, 0xa2, 0xb4, 0x05, 0x7b, 0x7b, 0xfb, 0x78,
-	0x0f, 0xbe, 0xb7, 0x03, 0x27, 0xf9, 0x2b, 0x71, 0xfb, 0xc1, 0xd5, 0x7b, 0xf9, 0x15, 0xd7, 0x0d,
-	0x6b, 0x46, 0x77, 0xcb, 0x0a, 0xe7, 0xe0, 0x3c, 0x6a, 0xd2, 0xab, 0x16, 0xaf, 0xc0, 0x6e, 0x4a,
-	0xed, 0x8b, 0x40, 0x44, 0xc7, 0xc9, 0x34, 0xde, 0xee, 0x0d, 0x89, 0x94, 0x8b, 0x52, 0x9a, 0x4c,
-	0xf8, 0x0c, 0x90, 0x72, 0x55, 0x65, 0xa4, 0x9b, 0xb7, 0x4f, 0x4c, 0x60, 0xdc, 0x70, 0xd7, 0xfa,
-	0x22, 0xb0, 0x23, 0x37, 0xb9, 0xd8, 0x69, 0x6e, 0x62, 0x71, 0x46, 0x5a, 0x72, 0x27, 0xfb, 0xec,
-	0xec, 0x1c, 0x9c, 0xe1, 0x8d, 0x08, 0xe3, 0x35, 0xa9, 0xa1, 0x6d, 0xc9, 0x5e, 0x87, 0x77, 0x70,
-	0x90, 0x91, 0x7e, 0x22, 0xb5, 0x2a, 0xd1, 0x03, 0xbb, 0xe1, 0xae, 0xc7, 0x9a, 0x48, 0x23, 0x8d,
-	0x93, 0xb3, 0xf2, 0xad, 0xc1, 0xc9, 0x59, 0x19, 0x67, 0x4d, 0xca, 0xb7, 0x03, 0x11, 0x59, 0xd2,
-	0xc8, 0xeb, 0x4b, 0x80, 0x0d, 0x34, 0x3a, 0x60, 0x2d, 0x1e, 0xbc, 0x11, 0x1e, 0xc2, 0xe4, 0x5e,
-	0xca, 0x85, 0xf4, 0x44, 0xf2, 0x2d, 0xc0, 0x4d, 0x0d, 0x68, 0xd6, 0x83, 0xe2, 0x2d, 0x1c, 0x2d,
-	0x6b, 0xc5, 0x54, 0xfc, 0x8e, 0x9a, 0xee, 0x99, 0x31, 0x3b, 0xfd, 0xe7, 0x67, 0xc2, 0x11, 0xde,
-	0x80, 0xbb, 0xac, 0x0b, 0xd2, 0xe5, 0x40, 0x7d, 0xb6, 0x93, 0xfa, 0x1b, 0xb3, 0xa7, 0xfc, 0xe2,
-	0xf4, 0x87, 0x99, 0xff, 0x04, 0x00, 0x00, 0xff, 0xff, 0xa8, 0x85, 0x9f, 0x4f, 0xad, 0x01, 0x00,
-	0x00,
+	// 358 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0xd1, 0x4b, 0xc2, 0x50,
+	0x14, 0xc6, 0xdd, 0xa6, 0x43, 0xcf, 0x2a, 0xec, 0x4a, 0x38, 0x46, 0x94, 0x8c, 0x1e, 0x56, 0x81,
+	0x0f, 0xf3, 0x31, 0x91, 0xc0, 0x7a, 0x10, 0x19, 0xc6, 0x15, 0x7b, 0x0c, 0x6e, 0xba, 0x68, 0x74,
+	0xdb, 0x91, 0xbb, 0xab, 0xd6, 0x7f, 0xd0, 0x9f, 0x1d, 0x77, 0x5b, 0xea, 0xc0, 0xf5, 0x76, 0xee,
+	0xd9, 0xef, 0xbb, 0xe7, 0x7c, 0xbb, 0x1f, 0x9c, 0xce, 0xdf, 0x19, 0x26, 0x9f, 0x18, 0x7f, 0x84,
+	0xdf, 0xdd, 0xa5, 0x40, 0x89, 0xc4, 0xda, 0x6b, 0xb9, 0x57, 0x60, 0x4e, 0x43, 0xb1, 0x0e, 0x05,
+	0x71, 0xa0, 0x9e, 0xa4, 0xd5, 0xe8, 0xc1, 0xd6, 0x3a, 0x9a, 0x57, 0xa3, 0xdb, 0xb3, 0xdb, 0x87,
+	0xe3, 0x27, 0x26, 0x64, 0x24, 0x23, 0x8c, 0x47, 0xf1, 0x1b, 0x92, 0x5b, 0x30, 0xb3, 0x8f, 0xb6,
+	0xd6, 0x31, 0x3c, 0xcb, 0x6f, 0x75, 0xf7, 0xe7, 0x64, 0x37, 0xd2, 0x1c, 0x71, 0x7b, 0x60, 0x4e,
+	0x25, 0x93, 0xab, 0x84, 0x5c, 0x83, 0x21, 0x42, 0x99, 0x5e, 0x7f, 0xe2, 0xb7, 0x8b, 0x9a, 0x94,
+	0x18, 0xe2, 0x22, 0xa4, 0x8a, 0x71, 0x5f, 0x00, 0x86, 0x18, 0xc7, 0x01, 0x93, 0x22, 0xfa, 0x22,
+	0x3e, 0x54, 0x05, 0x6e, 0x92, 0x7c, 0xda, 0x45, 0x41, 0xb9, 0xc3, 0xba, 0x01, 0x93, 0x14, 0x37,
+	0x34, 0x65, 0x9d, 0x73, 0x30, 0xb3, 0x33, 0x21, 0x50, 0x5d, 0x33, 0x9e, 0xa9, 0x75, 0x9a, 0xd6,
+	0xae, 0x07, 0x90, 0xad, 0xa9, 0x06, 0xff, 0x6b, 0xfe, 0x1e, 0xea, 0x01, 0x93, 0xcf, 0x8c, 0xaf,
+	0x42, 0xd2, 0x04, 0x43, 0xe0, 0x26, 0x47, 0x54, 0xa9, 0x3a, 0x73, 0xe4, 0xb6, 0x9e, 0x75, 0xe6,
+	0xc8, 0x55, 0x67, 0xcd, 0xb8, 0x6d, 0x74, 0x34, 0x4f, 0xa7, 0xaa, 0xbc, 0xb9, 0x04, 0xd8, 0xd9,
+	0x23, 0x26, 0xe8, 0x93, 0x71, 0xb3, 0x42, 0x1a, 0x50, 0x7b, 0xa4, 0x74, 0x42, 0x9b, 0x9a, 0xff,
+	0xa3, 0x83, 0x35, 0x54, 0x96, 0x82, 0xd4, 0x12, 0x19, 0xc0, 0xd1, 0x6c, 0xc9, 0x91, 0x2d, 0x72,
+	0xfb, 0xed, 0x12, 0xc3, 0x4e, 0xeb, 0xc0, 0x3f, 0x74, 0x2b, 0xe4, 0x0e, 0xac, 0xd9, 0x72, 0xc1,
+	0x64, 0x98, 0x6d, 0x7d, 0x56, 0xa0, 0xfe, 0xcc, 0x94, 0x89, 0xfb, 0x00, 0xe3, 0x88, 0xf3, 0x3c,
+	0x16, 0xed, 0x03, 0x2f, 0xab, 0xd0, 0x32, 0xf5, 0x00, 0x1a, 0xdb, 0xa8, 0x10, 0xa7, 0xc0, 0x14,
+	0x22, 0x54, 0xa2, 0x7f, 0x35, 0xd3, 0x90, 0xf6, 0x7e, 0x03, 0x00, 0x00, 0xff, 0xff, 0xca, 0x5d,
+	0x5f, 0x19, 0xb9, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -265,6 +392,8 @@ const _ = grpc.SupportPackageIsVersion4
 type ChaosMonkeyClient interface {
 	UploadMatrix(ctx context.Context, in *ConnMatrix, opts ...grpc.CallOption) (*Status, error)
 	UpdateValue(ctx context.Context, in *MatValue, opts ...grpc.CallOption) (*Status, error)
+	KillServer(ctx context.Context, in *ServerStat, opts ...grpc.CallOption) (*Status, error)
+	Partition(ctx context.Context, in *PartitionInfo, opts ...grpc.CallOption) (*Status, error)
 }
 
 type chaosMonkeyClient struct {
@@ -293,10 +422,47 @@ func (c *chaosMonkeyClient) UpdateValue(ctx context.Context, in *MatValue, opts 
 	return out, nil
 }
 
+func (c *chaosMonkeyClient) KillServer(ctx context.Context, in *ServerStat, opts ...grpc.CallOption) (*Status, error) {
+	out := new(Status)
+	err := c.cc.Invoke(ctx, "/chaosmonkey.ChaosMonkey/KillServer", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *chaosMonkeyClient) Partition(ctx context.Context, in *PartitionInfo, opts ...grpc.CallOption) (*Status, error) {
+	out := new(Status)
+	err := c.cc.Invoke(ctx, "/chaosmonkey.ChaosMonkey/Partition", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ChaosMonkeyServer is the server API for ChaosMonkey service.
 type ChaosMonkeyServer interface {
 	UploadMatrix(context.Context, *ConnMatrix) (*Status, error)
 	UpdateValue(context.Context, *MatValue) (*Status, error)
+	KillServer(context.Context, *ServerStat) (*Status, error)
+	Partition(context.Context, *PartitionInfo) (*Status, error)
+}
+
+// UnimplementedChaosMonkeyServer can be embedded to have forward compatible implementations.
+type UnimplementedChaosMonkeyServer struct {
+}
+
+func (*UnimplementedChaosMonkeyServer) UploadMatrix(ctx context.Context, req *ConnMatrix) (*Status, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UploadMatrix not implemented")
+}
+func (*UnimplementedChaosMonkeyServer) UpdateValue(ctx context.Context, req *MatValue) (*Status, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateValue not implemented")
+}
+func (*UnimplementedChaosMonkeyServer) KillServer(ctx context.Context, req *ServerStat) (*Status, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method KillServer not implemented")
+}
+func (*UnimplementedChaosMonkeyServer) Partition(ctx context.Context, req *PartitionInfo) (*Status, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Partition not implemented")
 }
 
 func RegisterChaosMonkeyServer(s *grpc.Server, srv ChaosMonkeyServer) {
@@ -339,6 +505,42 @@ func _ChaosMonkey_UpdateValue_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ChaosMonkey_KillServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ServerStat)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChaosMonkeyServer).KillServer(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chaosmonkey.ChaosMonkey/KillServer",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChaosMonkeyServer).KillServer(ctx, req.(*ServerStat))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ChaosMonkey_Partition_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PartitionInfo)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChaosMonkeyServer).Partition(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chaosmonkey.ChaosMonkey/Partition",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChaosMonkeyServer).Partition(ctx, req.(*PartitionInfo))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _ChaosMonkey_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "chaosmonkey.ChaosMonkey",
 	HandlerType: (*ChaosMonkeyServer)(nil),
@@ -350,6 +552,14 @@ var _ChaosMonkey_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UpdateValue",
 			Handler:    _ChaosMonkey_UpdateValue_Handler,
+		},
+		{
+			MethodName: "KillServer",
+			Handler:    _ChaosMonkey_KillServer_Handler,
+		},
+		{
+			MethodName: "Partition",
+			Handler:    _ChaosMonkey_Partition_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
