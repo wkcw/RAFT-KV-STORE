b2ead18f4256a0cb3994a721a708d83a95ffd982
RazorXu <594877110@qq.com>
2019-06-07 19:39:35 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 export GOPATH=$GOPATH:~/cse223b-RAFT-KV-STORE/
 rm -rf ~/cse223b-RAFT-KV-STORE/
-git clone https://SwimmingFish6:lxhxhd123@github.com/wkcw/cse223b-RAFT-KV-STORE.git -b master
 source /etc/profile
 cd cse223b-RAFT-KV-STORE/
 go build src/main/raftkv_main.go
